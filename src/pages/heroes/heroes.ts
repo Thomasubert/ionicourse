e74d26c12a433e556f7ef49ba0c8d65ee7b026bb
= <=>
2018-12-19 13:44:07 +0100

    premier comit

---
+++ src/pages/heroes/heroes.ts
@@ -0,0 +1,19 @@
+import {Component} from '@angular/core';
+import { NavController } from 'ionic-angular';
+import { SingleHeroPage } from './single-hero/single-hero';
+
+@Component({
+    selector: 'pages-heroes',
+    templateUrl: 'heroes.html'
+})
+
+export class HeroesPage{
+
+    constructor(private navCtrl: NavController) {}
+
+    onLoadHero(name: string){
+        this.navCtrl.push(SingleHeroPage, {heroName: name})
+
+    }
+
+}
--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
+import { HeroesPage } from '../heroes/heroes';
 
 @Component({
   selector: 'page-home',
@@ -9,6 +10,10 @@ export class HomePage {
 
   constructor(public navCtrl: NavController) {
 
+  }
+
+  onLoadHeroes(){
+    this.navCtrl.push(HeroesPage);
   }
 
 }
