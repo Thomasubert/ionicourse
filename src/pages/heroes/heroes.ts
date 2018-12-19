546cfb382f7f0caa9a6ba4156f3e6b4b67dd79ef
= <=>
2018-12-19 15:37:18 +0100

    save2

--- src/pages/heroes/heroes.ts
+++ src/pages/heroes/heroes.ts
@@ -9,7 +9,35 @@ import { SingleHeroPage } from './single-hero/single-hero';
 
 export class HeroesPage{
 
-    constructor(private navCtrl: NavController) {}
+
+    heroesList = [
+        {
+            name:'Superman',
+            description: [
+                'Ne supporte pas la kryptonite',
+                'Est copain avec Batman de temps en temps'
+            ]
+        },
+        {
+            name:'The Joker',
+            description: [
+                'Ennemi de Batman',
+                'Meurtrier à Gotham City'
+            ]
+        },
+        {
+            name:'Felix The Cat',
+            description: [
+                'A 7 vies',
+                'Revient à la maison que pour manger'
+            ]
+        },
+
+    ]
+
+    constructor(private navCtrl: NavController) {
+
+    }
 
     onLoadHero(name: string){
         this.navCtrl.push(SingleHeroPage, {heroName: name})
--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,5 +1,4 @@
 import { Component } from '@angular/core';
-import { NavController } from 'ionic-angular';
 import { HeroesPage } from '../heroes/heroes';
 
 @Component({
@@ -7,13 +6,5 @@ import { HeroesPage } from '../heroes/heroes';
   templateUrl: 'home.html'
 })
 export class HomePage {
-
-  constructor(public navCtrl: NavController) {
-
-  }
-
-  onLoadHeroes(){
-    this.navCtrl.push(HeroesPage);
-  }
-
+  lienVersHeroesPage = HeroesPage;
 }
